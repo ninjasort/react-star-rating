5194d5ced2726d298c661a15a11fedeb090efdec
Cameron J Roe <cameronjroe@gmail.com>
2015-03-08 19:45:02 -0400

    add github banner

--- dist/demo/bundle.js
+++ dist/demo/bundle.js
@@ -18421,7 +18421,7 @@ module.exports = require('./lib/React');
 },{"./lib/React":30}],149:[function(require,module,exports){
 module.exports={
   "name": "react-star-rating",
-  "version": "0.1.0",
+  "version": "1.0.0",
   "description": "A simple star rating component built with React.",
   "main": "dist/react-star-rating.min.js",
   "scripts": {},
