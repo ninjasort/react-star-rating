4490f8e5519880d3dcae6f05c3c62ebe97ed49b8
Cameron J Roe <cameronjroe@gmail.com>
2015-03-08 17:24:19 -0400

    finish distribution

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var rename = require('gulp-rename');
 var react = require('gulp-react');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
+var wrapAmd = require('gulp-wrap-amd');
 
 gulp.task('lint', function () {
   return gulp.src('./src/**/*.{jsx, js}')
@@ -28,7 +29,12 @@ gulp.task('default', ['lint','styles'], function () {
     .pipe(source('bundle.js'))
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist/demo'));
+});
+
+gulp.task('watch', ['default'], function () {
+  gulp.watch(['./src/*.js', './src/**/*.jsx', './src/sass/{*/,}*.scss'], ['default']);
+  return gulp.src('.').pipe(server());
 });
 
 gulp.task('styles', function () {
@@ -39,14 +45,49 @@ gulp.task('styles', function () {
     .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('watch', ['default'], function () {
-  gulp.watch(['./src/*.js', './src/**/*.jsx', './src/sass/{*/,}*.scss'], ['default']);
-  return gulp.src('.').pipe(server());
+gulp.task('build:styles', function () {
+  return gulp.src('./src/sass/react-star-rating.scss')
+    .pipe(sass({
+      includePaths: require('node-bourbon').includePaths,
+      outputStyle: 'compressed'
+    }))
+    .pipe(rename('react-star-rating.min.css'))
+    .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('build:react', function () {
+gulp.task('build:npm', function () {
   return gulp.src('./src/StarRating.jsx')
     .pipe(react())
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(rename('react-star-rating.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(rename('react-star-rating.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('uglify', function () {
+  return gulp.src('./dist/browser/react-star-rating.js')
+    .pipe(rename('react-star-rating.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist/browser'));
 });
+
+gulp.task('build:bower', function () {
+  return gulp.src('./src/StarRating.jsx')
+    .pipe(react())
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(rename('react-star-rating.js'))
+    .pipe(wrapAmd())
+    .pipe(gulp.dest('dist/browser'))
+    .pipe(rename('react-star-rating.min.js'))
+    .pipe(uglify())
+    .on('error', function (err) {
+      console.log(err.lineNumber, err);
+    })
+    .pipe(gulp.dest('dist/browser'));
+});
+
+gulp.task('dist', ['default', 'build:styles', 'build:npm', 'build:bower']);
